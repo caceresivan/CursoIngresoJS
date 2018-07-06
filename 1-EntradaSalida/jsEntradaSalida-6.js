dfc1f6d6dd68875dfcac6df31bd0de17a1462ad0
caceresivan <40808730+caceresivan@users.noreply.github.com>
2018-07-06 14:51:42 -0300

    Ejercicio 6 de js
    Intente hacer el ejercicio 6 pero no entiendo lo de parseint

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,10 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-
+	/*var numero Uno
+	var numero Dos
+	numero Uno = document.getElementById('numeroUno').value;
+	numero Dos = document.getElementById('numeroDos').value;
+	*/
 }
 
