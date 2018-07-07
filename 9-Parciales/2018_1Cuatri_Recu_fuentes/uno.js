8f096fcea13248437b4225528c7852e89b2ffb26
caceresivan <40808730+caceresivan@users.noreply.github.com>
2018-07-06 22:22:24 -0300

    ejercicio 1 parcial recuperatorio
    practicar mas ejercicios

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,17 +1,23 @@
-
+/*se ingresan tres numeros,
+se muestra la suma de los tres,
+el promedio de los tres y la
+resta de los tres en un solo alert
+*/
 function mostrar()
 {
-	/* esto esta mal
-	var base;
-	var altura;
-	var superficie;
-	var perimetro;
-	base=prompt("ingrese base: ");
-	altura=prompt("ingrese altura: ");
-	base=parseInt(base);
-	altura=parseInt(altura);
-	superficie= base+altura;
-	perimetro=base+altura;
-	alert("la superficie es: "+superficie + " y el perimetro es: "+perimetro);
-	*/
+	var numerouno;
+	var numerodos;
+	var numerotres;
+	var suma;
+	var promedio;
+	var resta;
+	numerouno=prompt("ingrese numero uno");
+	numerodos=prompt("ingrese numero dos");
+	numerotres=prompt("ingrese numero tres");
+	numerouno=parseInt(numerouno);
+	numerodos=parseInt(numerodos);
+	numerotres=parseInt(numerotres);
+	suma=numerouno+numerodos+numerotres;
+	promedio=numerouno+numerodos+numerotres/3
+	
 }
