98e52f45f5744d021ec3a115f8d98b6659f11508
caceresivan <40808730+caceresivan@users.noreply.github.com>
2018-07-06 21:58:58 -0300

    parciales vistos 06/07/2018
    practicar estos ejercicios y entender mejor los enunciados

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,4 +1,8 @@
 function mostrar()
 {
-  
+  var nombre;
+  var localidad;
+  nombre=prompt(elNombre);
+  localidad=prompt(laLocalidad);
+  alert("usted es: "+nombre,"y vive en la localidad de: "+localidad);
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,17 @@
 
 function mostrar()
 {
-
+	/* esto esta mal
+	var base;
+	var altura;
+	var superficie;
+	var perimetro;
+	base=prompt("ingrese base: ");
+	altura=prompt("ingrese altura: ");
+	base=parseInt(base);
+	altura=parseInt(altura);
+	superficie= base+altura;
+	perimetro=base+altura;
+	alert("la superficie es: "+superficie + " y el perimetro es: "+perimetro);
+	*/
 }
