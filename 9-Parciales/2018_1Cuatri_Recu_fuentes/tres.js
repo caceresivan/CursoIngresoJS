3f861ad401b64669873f92a6a5dc6b2d82c17d7d
caceresivan <40808730+caceresivan@users.noreply.github.com>
2018-07-10 21:57:41 -0300

    ejercicios terminados en clase
    ejercicios del 1 al 3 de parciales recuperatorio

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,22 @@
 function mostrar()
 {
+	var precio;
+	var porcentaje;
+	var descuento;
+	var preciocondescuento;
+	var Iva;
+	var soloIva;
 
+	precio=prompt("Ingrese un precio:");
+	porcentaje=prompt("Ingrese un porcentaje");
+	precio=parseInt(precio);
+	porcentaje=parseInt(porcentaje);
+
+	descuento=precio*porcentaje/100;
+	preciocondescuento=precio-descuento;
+	Iva=preciocondescuento*1.21;
+	soloIva=Iva-preciocondescuento;
+
+	alert("El decuento en dinero es: "+descuento+", el precio con descuento es: "+preciocondescuento+" y el iva es: "+soloIva);
+	document.getElementById('elPrecioFinal').value=Iva;
 }
