cbec02058d2bff416389e5785d5266fe9f11808e
caceresivan <40808730+caceresivan@users.noreply.github.com>
2018-07-10 22:01:11 -0300

    ejercicios terminados en clase 10/07/2018
    ejercicios de IF del 1 al 6

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,19 @@
 function mostrar()
 {
 //tomo la edad  
+	var edad;
+	edad=document.getElementById('edad').value;
 
-
+	if (edad<12 || edad>17)
+	{
+		alert("Usted no es un adolescente");
+	}
+	/*if (edad<12) 
+	{
+		alert("Usted no es adolescente");
+	}
+	if (edad>17) 
+	{
+		alert("Usted no es adolescente");
+	}*/
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,61 @@
 function mostrar()
 {
-//tomo la edad  
+	//tomo la edad  
+		/*Forma Pro mejorada*/
+
+		var edad;
+	edad=document.getElementById('edad').value;
+
+	if (edad<13)
+	{
+		alert("Usted es menor");
+	}
+	else
+	{
+		if (edad>17)
+		{
+			alert("Usted es mayor");
+
+		}else
+			alert("Usted es adolescente");
+	}
+
+	/*
+	forma "Disney"
 
 
+	var edad;
+	edad=document.getElementById('edad').value;
 
+	if (edad<13)
+	{
+		alert("Usted es un niño");
+	}
+	if (edad>12 && edad<18)
+	{
+		alert("Usted es adolescente");
+	}
+	if (edad>17)
+	{
+		alert("Usted es mayor de edad");
+	}*/
 
+	/*
+	bien echo pero forma mas dificil
+
+	var edad;
+	edad=document.getElementById('edad').value;
+
+	if (edad<13)
+	{
+		alert("Usted es un niño");
+	}else
+	{
+		if (edad>12 && edad<18)
+		{
+			alert("Usted es adolescente");
+		}else
+		{
+			alert("Usted es mayor");
+		}*/
 }//FIN DE LA FUNCIÓN
