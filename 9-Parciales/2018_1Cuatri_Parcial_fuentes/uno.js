08ebfb34efa9ff98ade064a3a752fafb14f1f620
caceresivan <40808730+caceresivan@users.noreply.github.com>
2018-07-10 21:59:20 -0300

    ejercicios terminados en clase
    ejercicio 1 del parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,16 @@
 
 function mostrar()
 {
+	var largo;
+	var ancho;
+	var perimetro;
 
+	largo=prompt("Ingrese el largo de un rectangulo:");
+	ancho=prompt("Ingrese el ancho de un rectangulo:");
+	largo=parseInt(largo);
+	ancho=parseInt(ancho);
+
+	perimetro=largo*2+ancho*2;
+
+	alert("El perimetro es: "+perimetro);
 }
