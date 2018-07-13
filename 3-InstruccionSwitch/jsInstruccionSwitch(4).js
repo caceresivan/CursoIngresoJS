34b00830169422dd72b8535e3daf8220fe02d501
caceresivan <40808730+caceresivan@users.noreply.github.com>
2018-07-13 20:25:19 -0300

    ejercicios instruccion Switch
    ejercicios terminados del 1 al 6 instruccion switch

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -5,7 +5,29 @@ var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
-	
+	switch(mesDelAño)
+	{
+		case"Febrero":
+			alert("Este mes tiene 28 dias.");
+				break;
+
+		case"Abril":
+		case"Junio":
+		case"Septiembre":
+		case"Noviembre":
+			alert("Este mes tienen 30 dias.");
+				break;
+
+		case"Enero":
+		case"Marzo":		
+		case"Mayo":
+		case"Julio":
+		case"Agosto":
+		case"Octubre":
+		case"Diciembre":
+			alert("Este mes tienen 31 dias.");
+				break;
+	}
 
 
 
