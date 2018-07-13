8c582c265a9f277ac61c497f78c8a99e70d26044
caceresivan <40808730+caceresivan@users.noreply.github.com>
2018-07-13 18:48:48 -0300

    ejercicios 2 y 3 del parcial
    ejercicios terminados

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,14 @@
 function mostrar()
 {
+	var precio;
+	var porcentaje;
+	var preciofinal;
 
+	precio=prompt("Ingrese el precio:");
+	porcentaje=prompt("Ingrese el porcentaje de descuento:");
+	precio=parseInt(precio);
+	porcentaje=parseInt(porcentaje);
+	preciofinal=precio*porcentaje/100;
+
+	document.getElementById('elPrecioFinal').value=preciofinal;
 }
