adf6e92b105718a0d2863ecfffc2e204b14ff9f0
caceresivan <40808730+caceresivan@users.noreply.github.com>
2018-07-27 21:57:04 -0300

    instruccion IF
    terminar ejercicios 7 al 10

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -5,14 +5,40 @@ function mostrar()
 	//declarar contadores y variables 
 	
 	var respuesta="si";
-
-	document.write("hola");
+	var sumaNegativos=0;
+	var sumaPositivos=0;
+	var contPositivos=0;
+	var contNegativos=0;
+	var contCeros=0;
+	var contPares=0;
+	var promedioPositivos;
+	var promedioNegativos;
+	var diferenciaPosyNeg;
+	var numeroIngresado;
+	var acumPositivos=0;
 
 	while(respuesta!="no")
 	{
-		
-	
-	}
+		numeroIngresado=prompt("Ingrese un numero.");
+		numeroIngresado=parseInt(numeroIngresado);
+
+		while(isNaN(numeroIngresado))
+		{
+			numeroIngresado=prompt("Ingrese un numero.");
+			numeroIngresado=parseInt(numeroIngresado);
+		}
+		if(numeroIngresado>0)
+		{
+			contPositivos++;
+			acumPositivos=numeroIngresado;
+		}else if(numeroIngresado<0)
+		{
+			contNegativos++;
+		}else()
+		{
+			contCeros++;
+		}
+	}//while(respuesta)
 
 
 
--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -4,7 +4,20 @@ function mostrar()
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
+	var numeroIngresado;
+	var promedio;
 
+	while(respuesta != "no")
+	{
+
+	numeroIngresado=prompt("Ingrese un numero.");
+	numeroIngresado=parseInt(numeroIngresado);
+
+	acumulador=acumulador+numeroIngresado;
+	contador++;
+	respuesta=prompt("Quiere continuar?");
+	}
+	
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
