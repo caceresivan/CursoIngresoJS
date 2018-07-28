6b813f7ed377764e9bd5b363a83b006994faef02
caceresivan <40808730+caceresivan@users.noreply.github.com>
2018-07-27 22:11:09 -0300

    parcial ejercicio 7
    rehacer el ejercicio 7 del parcial y revisarlo a fondo que no tenga ningun error

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,63 @@
+/*
+maria f 15
+jose m 33
+fer f 25
+*/
 function mostrar()
 {
+	var contador=0;
+	var nombre;
+	var sexo;
+	var edad;
+	var cantMujeres=0;
+	var cantVarones=0;
+	var cantMenores=0;
+	var cantMayores=0;
+	var suma;
 
+	while(contador<3)
+	{
+		
+		nombre=prompt("Ingrese un nombre.");
+		sexo=prompt("Ingrese su sexo.");
+
+		while(sexo!= "f" && sexo!= "m")
+		{
+			sexo=prompt("Error, ingrese su sexo.");
+		}
+
+		edad=prompt("Ingrese su edad.");
+		edad=parseInt(edad);
+		while(isNaN(edad) || edad<0 && edad>100)
+		{
+			edad=prompt("Error, ingrese su edad.");
+			edad=parseInt(edad);
+		}
+		contador++;
+		
+	}
+	if(sexo=="f")
+	{
+		if(cantMujeres==1)
+		{
+	
+		}
+		cantMujeres++;	
+	}
+	else
+	{
+		cantVarones++;
+	}
+	if(edad<18 && edad>0)
+	{
+		cantMenores++;
+	}
+	else if(edad>17 && edad<100)
+	{
+		cantMayores++;
+	}
+
+	document.write("1.<br>Cantidad de mujeres= "+cantMujeres+"<br>Cantidad de varones= "+cantVarones+"<br>Cantidad de menores= "+cantMenores+"<br>Cantidad de mayores= "+cantMayores);
+
+//esta todo mal;
 }
