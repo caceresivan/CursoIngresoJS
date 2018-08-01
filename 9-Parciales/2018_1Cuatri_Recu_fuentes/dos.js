239b017433e3f81147cb733e9d81af465a0ef141
caceresivan <40808730+caceresivan@users.noreply.github.com>
2018-07-31 22:12:30 -0300

    ejercicios 1 y 2 parcial recuperatorio
    ejercicios terminados

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,8 +1,22 @@
+/*
+tomar los datos de los cuadros de texto y formar la frase
+"Yo soy xxxx, vivo en al localidad de xxxxxxx
+y estudio en la UTN"
+
+
+
+*/
+
+
 function mostrar()
 {
-  var nombre;
-  var localidad;
-  nombre=prompt(elNombre);
-  localidad=prompt(laLocalidad);
-  alert("usted es: "+nombre,"y vive en la localidad de: "+localidad);
+	var nombre;
+	var localidad;
+
+	nombre=document.getElementById('elNombre').value;
+	localidad=document.getElementById('laLocalidad').value;
+
+	alert("Yo soy "+nombre+" ,vivo en la localidad de "+localidad+" y estudio en la UTN.");
+	
+
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -5,19 +5,19 @@ resta de los tres en un solo alert
 */
 function mostrar()
 {
-	var numerouno;
-	var numerodos;
-	var numerotres;
-	var suma;
-	var promedio;
-	var resta;
-	numerouno=prompt("ingrese numero uno");
-	numerodos=prompt("ingrese numero dos");
-	numerotres=prompt("ingrese numero tres");
-	numerouno=parseInt(numerouno);
-	numerodos=parseInt(numerodos);
-	numerotres=parseInt(numerotres);
-	suma=numerouno+numerodos+numerotres;
-	promedio=numerouno+numerodos+numerotres/3
-	
+	var base;
+	var altura;
+	var perimetro;
+	var superficie;
+
+	base=prompt("Ingrese base.");
+	altura=prompt("Ingrese altura.");
+
+	base=parseInt(base);
+	altura=parseInt(altura);
+
+	perimetro=base*3;
+	superficie=(base*altura)/2;
+
+	alert("La superficie es: "+superficie+" y el perimetro es: "+perimetro);
 }
