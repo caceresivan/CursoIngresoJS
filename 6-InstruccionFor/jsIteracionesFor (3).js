09f3b9e0839ef2ca0ec2160be369cc83f9c49698
caceresivan <40808730+caceresivan@users.noreply.github.com>
2018-08-03 22:07:27 -0300

    ejercicio 3 iteracion IF
    ejercicio 3 IF terminado en clase

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,7 +1,22 @@
+/*
+al presionar el botón 
+pedir la cantidad de veces que quiero 
+repetir el mensaje "Hola UTN FRA"
+*/
+
 function mostrar()
 {
 
-var repetciones = prompt("ingrese el número de repeticiones");
+	var repeticiones=prompt("ingrese el número de repeticiones");
+	var contador;
+
+	
+	repeticiones=parseInt(repeticiones);
+
+	for(contador=0;contador<repeticiones;contador++)
+	{
+		console.log("Hola UTN FRA.");
+	}
 
 
 
