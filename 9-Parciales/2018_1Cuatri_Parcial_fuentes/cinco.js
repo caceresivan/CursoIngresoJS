14e14088eed261742fe527dae4f71bee026bf9a0
caceresivan <40808730+caceresivan@users.noreply.github.com>
2018-08-03 21:47:42 -0300

    ejercicios 4 y 5 parcial
    ejercicios 4 y 5 terminados en clase

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,41 @@
+/*
+Bienvenidos. 
+(SWITCH)pedir el ingreso de un planeta del sistema solar 
+Si es la tierra mostrar "acá vivimos". 
+Si está más cerca del sol, "acá hace más calor". 
+Si está más lejos del sol, "acá hace más frio". 
+Si no es un planeta valido informarlo. 
+Aclaración: los planetas se ingresan en minúscula (Mercurio y Venus están antes que la tierra).
+
+*/
+
 function mostrar()
 {
+	var planeta;
+	var
 
+	planeta=prompt("Ingrese el nombre de un planeta del sistema solar.");
+
+	switch(planeta){
+
+		case "tierra":
+			alert("acá vivimos.");
+				break;
+
+		case "mercurio":
+		case "venus":
+			alert("acá hace más calor.");
+				break;
+
+		case "marte":
+		case "jupiter":
+		case "saturno":
+		case "urano":
+		case "neptuno":
+		case "pluton":
+			alert("acá hace más frio.");
+				break;
+		default:
+			alert("No es un planeta válido.");						
+	}
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,41 @@
+/*
+Bienvenidos. 
+(IF)Pedir dos números y mostrar el resultado:
+Si son iguales los muestro concatenados. 
+Si el primero es mayor, los resto, 
+de lo contrario los sumo. 
+Si la suma es mayor a 10 ,además de mostrar el resultado, muestro el mensaje 
+"la suma es xxx y supero el 10".
+*/
+
+
 function mostrar()
 {
+	var numeroUno;
+	var numeroDos;
+	var suma;
+	var resta;
 
+	numeroUno=prompt("Ingrese un numero.");
+	numeroUno=parseInt(numeroUno);
+	numeroDos=prompt("Ingrese un numero.");
+	numeroDos=parseInt(numeroDos);
+
+	if(numeroUno==numeroDos){
+		alert("El resultado es: "+numeroUno+numeroDos);
+	}
+	else if(numeroUno>numeroDos){
+		resta=numeroUno-numeroDos;
+			alert("El resultado es: "+resta);
+	}else{
+		suma=numeroUno+numeroDos;
+		if(suma>10){
+			alert("El resultado es: "+suma+" y supero el 10.");
+		}
+		else{
+			alert("El resultado es: "+suma);
+		}	
+
+	}//else
+	
 }
